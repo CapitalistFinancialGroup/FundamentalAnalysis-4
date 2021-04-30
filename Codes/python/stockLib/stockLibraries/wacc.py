6ab0595875b2afefd5f133fb92bc7b66f2709cb9
Sudeshna Bora <sudeshnamani1@gmail.com>
2021-04-30 22:50:24 +0530

    Adds the functionality to calculate rate of return of risk free asset
    Closes #19

--- Codes/python/stockLib/stockLibraries/wacc.py
+++ Codes/python/stockLib/stockLibraries/wacc.py
@@ -12,6 +12,10 @@ import stockLibraries.CAPMModel as capm
 import stockLibraries.DividendDetails as dd
 import stockLibraries.FinancialDetails as fd
 import pandas as pd
+import datetime
+from dateutil.relativedelta import relativedelta
+import requests
+from bs4 import BeautifulSoup
 
 def calculate_cod(balance_df,income_df):
     """
@@ -170,6 +174,65 @@ def calculate_wacc_dividend(nseId,current_stock_price,outstanding_shares):
     wacc_marginal_tax_rate = percentage_equity*roe/10000 + cod/100*percentage_debt/100*(1-tax_rate/100)
     
     return wacc_default_tax_rate*100,wacc_marginal_tax_rate*100
+
+
+def risk_free_return():
+    """
+    Calculates the rate of return of a risk free commodity.
+    Currently the 1 year daily historical return rate of Indian Government 91 days T bill 
+    is considered. 
+
+    Returns
+    -------
+    risk_free_return_rate : float
+        The rate of return of a risk free commodity.
+
+    """
+    
+    
+    #Step 1: Fetch the current dates. 
+    
+    current_date = datetime.datetime.now() - relativedelta(days=1)
+    previous_date = current_date - relativedelta(years=1)
+    
+    current_date_str = current_date.strftime("%d/%m/%Y")
+    previous_date_str = previous_date.strftime("%d/%m/%Y")
+    
+    #Step 2 : Create the header and data for extracting information
+    
+    data = {'curr_id': '24003','smlID': '203924','header': 'India 3-Month Bond Yield Historical Data','st_date': previous_date_str,
+            'end_date': current_date_str,'interval_sec': 'Daily','sort_col': 'date','sort_ord': 'DESC','action': 'historical_data'
+            }
+    
+    headers = {
+    'authority': 'in.investing.com',
+    'accept': 'text/plain, */*; q=0.01',
+    'x-requested-with': 'XMLHttpRequest',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+    'content-type': 'application/x-www-form-urlencoded',
+    'sec-gpc': '1',
+    'origin': 'https://in.investing.com',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://in.investing.com/rates-bonds/india-3-month-bond-yield-historical-data',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'cookie': '__cfduid=d3c2e1fe62f9856cf29821a05e88b54811619441949; _tz_id=c528baa594cfdfaf5e93a86048b5c4f4; PHPSESSID=8h8jqnmeg1f2o5j03vo1nk7llr; SideBlockUser=a%3A2%3A%7Bs%3A10%3A%22stack_size%22%3Ba%3A1%3A%7Bs%3A11%3A%22last_quotes%22%3Bi%3A8%3B%7Ds%3A6%3A%22stacks%22%3Ba%3A1%3A%7Bs%3A11%3A%22last_quotes%22%3Ba%3A1%3A%7Bi%3A0%3Ba%3A3%3A%7Bs%3A7%3A%22pair_ID%22%3Bs%3A5%3A%2224003%22%3Bs%3A10%3A%22pair_title%22%3Bs%3A0%3A%22%22%3Bs%3A9%3A%22pair_link%22%3Bs%3A37%3A%22%2Frates-bonds%2Findia-3-month-bond-yield%22%3B%7D%7D%7D%7D; geoC=IN; adBlockerNewUserDomains=1619441951; gtmFired=OK; StickySession=id.6100660358.631in.investing.com; udid=f54e7210e8e3bc9b35a468ba65a8ad14; __cflb=02DiuF9qvuxBvFEb2qB1HcuDLvqD9ieP4QJ9YHcUfbsS4; G_ENABLED_IDPS=google; _fbp=fb.1.1619441955314.16268788; adsFreeSalePopUp=2; comment_notification_202451988=1; adsFreeSalePopUpa41d6b177ab422862f5e64f395319b37=1; r_p_s_n=1; smd=f54e7210e8e3bc9b35a468ba65a8ad14-1619454509; UserReactions=true; ses_id=ZSthIGJtNj4%2BempsN2YyMD5tN2g%2FPTs4Zm43PDA%2Fbng3I2RqYDc%2FeTE%2BbiBjYGZ6YmMyMjQ3YDA8PjJsNjZmN2UzYTRiZjY8PmtqMzdhMmE%2BPjdtPz87bWZlN2cwY24wNzdkZGBvP28xYm42Y2tmaGJwMi40cGBxPG4yYjZ3ZiFlamEgYjI2aj5gamA3MTJlPmo3ZT9tOzhmZzcyMGJudjd8; nyxDorf=ZWRmPDF5ZDo%2FaT02biNjY2IyYjgyK2dnMjBvaQ%3D%3D',
+    }
+    
+    req = requests.post('https://in.investing.com/instruments/HistoricalDataAjax', headers=headers, data=data)
+    soup = BeautifulSoup(req.content, "lxml")
+    table = soup.find('table', attrs={'id':'curr_table'})
+    df = pd.read_html(str(table))[0]
+    
+    #Step 3 : calculating the rate
+    
+    print("Fetched data, calculating the rate of risk free return")
+
+    risk_free_return_rate = df['Price'].mean()
+    
+    return risk_free_return_rate
+
     
     
     
