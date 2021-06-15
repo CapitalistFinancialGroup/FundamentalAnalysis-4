5271187924507810fe820ec261946b9e3177e075
Sudeshna Bora <sudeshnamani1@gmail.com>
2021-06-15 23:57:24 +0200

    Add util functionality
    Previous commit #72b2583001402901bffa9f3810d4cab882309209 did not
    push the functionalities.

---
+++ Codes/python/stockLib/stockLibraries/helper/util.py
@@ -0,0 +1,45 @@
+"""
+=========
+Helper functions
+=========
+This file includes functionalities to aide the library in its functionality.
+"""
+import yaml
+import logging
+
+def __prepare_configuration() -> dict:
+    """
+    Reads and prepares the configuration
+    Returns
+    -------
+    config (dict) : Dictionary containing the configurations
+    """
+
+    logging.info("Reading configuration file")
+
+    with open('../resources/configuration.yaml') as file:
+        config = yaml.load(file)
+
+    return config
+
+config = __prepare_configuration()
+
+def resolve_config_value(keys):
+    """
+    Resolves and returns the particular configuration value
+    Parameters
+    ----------
+    keys : @list
+           List of keys as it is embedded
+
+    Returns
+    -------
+    @obj : the configuration value
+    """
+
+    config_value = config
+    for key in keys:
+        config_value = config_value[key]
+
+    return config_value
+
